012940d88bd2dc860cbd28fe4da50eb5965eb69b
abhisheksuran <abhishek.suran23dec@gmail.com>
2019-06-22 04:19:23 -0400

    Add files via upload

---
+++ rename.py
@@ -0,0 +1,9 @@
+import glob, os
+
+def rename(dir, pattern, titlePattern):
+    for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
+        title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+        os.rename(pathAndFilename, 
+                  os.path.join(dir, titlePattern % title + ext))
+
+rename(r'path to the folder/Dog' , r'*.jpg', r'dog(%s)')
